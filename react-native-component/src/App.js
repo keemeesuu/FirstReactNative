d05bacd6eb677dfb5ff0ba0a683e223de725505e
keemeesuu <kimhogy@gmail.com>
2021-07-24 14:57:52 +0900

    ~

--- react-native-component/src/App.js
+++ react-native-component/src/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import Counter from './components/Counter';
 import MyButton from './components/MyButton';
 
 const App = () => {
@@ -12,12 +13,7 @@ const App = () => {
       justifyContent: 'center',
     }}
     >
-      <Text style={{ fontSize: 30, marginBottom: 0}}>
-        My Button Component
-      </Text>
-      <MyButton title="Button" name="BtnName" onPress={() => alert('props')} />
-      <MyButton title="Button" name="BtnName" onPress={() => alert('children')} >Children Props</MyButton>
-      <MyButton onPress={() => alert('default')} />
+      <Counter />
     </View>
   );
 };
---
+++ react-native-component/src/components/Counter.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import Mybutton from './MyButton';
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+    const [double, setDouble] = useState(0);
+    return (
+        <View style={{ alignItems: 'center'}}>
+            <Text style={{ fontSize: 30, margin: 10}}>count : {count}</Text>
+            <Text style={{ fontSize: 30, margin: 10}}>double : {double}</Text>
+            <Mybutton 
+                title="+1" 
+                onPress={() => {
+                    setCount(count + 1);
+                    setDouble(double + 2);
+                }} 
+            />
+            <Mybutton 
+                title="-1" 
+                onPress={() => {
+                    setCount(count - 1);
+                    setDouble(double - 2);
+                }} />
+        </View>
+    )
+}
+
+export default Counter;
